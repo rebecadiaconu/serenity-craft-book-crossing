08155aa71018cff1de61165367091ea27f22a8d8
Rebeca Diaconu <rebeca.diaconu15@s.unibuc.ro>
2021-04-21 19:12:44 +0300

    upload profile image for authenticated user - done

--- backend/functions/handlers/users.js
+++ backend/functions/handlers/users.js
@@ -87,3 +87,57 @@ exports.logIn = (req, res) => {
         return res.status(403).json({ general: 'Wrong credentials... Please try again!' });
     });
 };
+
+
+// Upload profile image for current user
+exports.uploadImage = (req, res) => {
+    const BusBoy = require('busboy');
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
+
+    const busboy = BusBoy({ headers: req.headers });
+
+    let imageUrl;
+    let imageFileName;
+    let imageToBeUploaded = {};
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        if (mimetype !== 'image/jpeg' && mimetype !== 'image/jpg' && mimetype !== 'image/png') {
+            return res.status(400).json({ error: 'Wrong file type submitted!' });
+        }
+
+        const imageExtension = filename.split('.')[filename.split('.').length - 1];
+        imageFileName = `${Math.round(Math.random()*1000000000000)}.${imageExtension}`;
+        const filepath = path.join(os.tmpdir(), imageFileName);
+
+        imageToBeUploaded = { filepath, mimetype };
+
+        file.pipe(fs.createWriteStream(filepath));
+    });
+
+    busboy.on('finish', () => {
+        admin.storage().bucket().upload(imageToBeUploaded.filepath, {
+            resumable: false,
+            metadata: {
+                metadata: {
+                    contentType: imageToBeUploaded.mimetype
+                }
+            }
+        })
+        .then(() => {
+            imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+
+            return db.doc(`/users/${req.user.username}`).update({ imageUrl });
+        })
+        .then(() => {
+            return res.json({message: 'Image uploaded successfully!'});
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({error: err.code});
+        });
+    });
+
+    busboy.end(req.rawBody);
+};
--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const app = require('express')();
 
-const { signUp, logIn } = require('./handlers/users');
+const { signUp, logIn, uploadImage } = require('./handlers/users');
 const fbAuth = require("./util/fbAuth");
 
 
@@ -9,7 +9,7 @@ const fbAuth = require("./util/fbAuth");
 app.post('/signup', signUp);        // Sign up with email and password route
 app.post('/login', logIn);        // Log in with email and password route
 // TODO:
-// app.post('/user/image', fbAuth, uploadImage);        // Upload user profile picture
+app.post('/user/image', fbAuth, uploadImage);        // Upload user profile picture
 // app.post('/user', fbAuth, addUserDetails);       // Add user details
 // app.get('/user', fbAuth,  getAuthenticatedUser);         // Get current user details
 // app.get('/user/:username', getUserDetails);          // Get any user details
