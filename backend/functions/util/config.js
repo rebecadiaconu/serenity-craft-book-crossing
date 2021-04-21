d4db996893986316d376d9dd592cf47a17d198b5
Rebeca Diaconu <rebeca.diaconu15@s.unibuc.ro>
2021-04-21 18:31:00 +0300

    added the main structure for users crud

---
+++ backend/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCClMWPcK2oi72BhAVxPDvmhKJfMpitP8o",
+    authDomain: "serenity-craft-liceapp.firebaseapp.com",
+    projectId: "serenity-craft-liceapp",
+    storageBucket: "serenity-craft-liceapp.appspot.com",
+    messagingSenderId: "1072054297819",
+    appId: "1:1072054297819:web:0eb64b9e68062b765ad8fe",
+    measurementId: "G-E737QGJN9F"
+};
